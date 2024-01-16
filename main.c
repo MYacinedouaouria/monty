5c4ab658d4d672715f4a561d91b70fcafd805753
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-16 08:56:48 -0800

    added main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void	main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+}
