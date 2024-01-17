0aebd064ad044a600c6d10ac46a4ba3d23cf9539
bikriira <iradufashabikri@gmail.com>
2024-01-17 02:06:59 -0800

    [Clean] Remove vi unwanted files and add a .gitignore

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "monty.h"
+
+
 /**
  *
  *
@@ -15,6 +17,21 @@ void	main(int argc, char *argv[])
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	else
+	{
+		FILE *f_ptr = fopen(argv[1], "r");
+		char buf[127];
+		int line_number = 1;
 
-
+		if (f_ptr == NULL)
+		{
+			printf("Error: Can't open file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+		while(fgets(buf, 127, f_ptr))
+		{
+			
+			line_number++;
+		}
+	}
 }
