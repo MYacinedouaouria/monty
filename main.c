d2853084fc2ef2fe7d32973720ec998f0a038f66
bikriira <iradufashabikri@gmail.com>
2024-01-17 15:46:46 -0800

    [Fix] show correct error if for any reason, it’s not possible to open

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int	main(int argc, char *argv[])
 
 		if (f_ptr == NULL)
 		{
-			fprintf(stderr, "Error Can't open file %s\n", argv[1]);
+			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 			exit(EXIT_FAILURE);
 		}
 		while (fgets(buf, 1024, f_ptr))
